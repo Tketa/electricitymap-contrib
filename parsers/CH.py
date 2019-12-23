f7c2c3054aca9407ce4cfa6ec4bf04d4a444c1b1
Jérémy Gotteland <jeremy.gotteland@gmail.com>
2019-12-24 00:21:17 +0100

    Add to exchanges and handle inputs properly

--- parsers/CH.py
+++ parsers/CH.py
@@ -9,12 +9,19 @@ import json
 
 EXCHANGE_URL = 'https://www.swissgrid.ch/bin/services/apicache?path=/content/swissgrid/fr/home/operation/grid-data/current-data/jcr:content/parsys/livedatawidget_copy'
 
+EXCHANGES_ID_MAPPING = {
+    'CH->FR': 'fr',
+    'CH->DE': 'de',
+    'AT->CH': 'at',
+    'CH->IT-NO': 'it',
+}
+
 # We use the arrow direction to determine if we are exporting or importing energy
 ZONE_EXPORT_ARROW_DIRECTION = {
-    'de': 'up',
-    'it': 'down',
-    'fr': 'left',
-    'at': 'right'
+    'DE': 'up',
+    'IT-NO': 'down',
+    'FR': 'left',
+    'AT': 'right'
 }
 
 def format_zone_keys(zone_key1, zone_key2, is_exporting):
@@ -23,8 +30,8 @@ def format_zone_keys(zone_key1, zone_key2, is_exporting):
 def is_exporting(zone_key, arrow_direction):
     return ZONE_EXPORT_ARROW_DIRECTION[zone_key] == arrow_direction
 
-def parse_flow(flow_string):
-    return float(flow_string.split(' ')[0])
+def parse_flow(flow_string, is_exporting):
+    return float(flow_string.split(' ')[0])*(1 if is_exporting else -1)
 
 def fetch_exchange(zone_key1, zone_key2, session=None, target_datetime=None, logger=None):
     """Requests the last known power exchange (in MW) between two zones
@@ -45,18 +52,24 @@ def fetch_exchange(zone_key1, zone_key2, session=None, target_datetime=None, log
     where net flow is from DK into NO
     """
     now = arrow.utcnow().format()
+    non_ch_key = zone_key1 if zone_key2 == 'CH' else zone_key2
+
+    sorted_zone_keys = sorted([zone_key1, zone_key2])
+    key = '->'.join(sorted_zone_keys)
+
+    id = EXCHANGES_ID_MAPPING[key] 
 
     r = session or requests.session()
     response = r.get(EXCHANGE_URL)
     data = json.loads(response.text)['data']['marker']
-    result = map(lambda x: {
-        'sortedZoneKeys': format_zone_keys('CH', x['id'].upper(), is_exporting(x['id'], x['direction'])),
+    result = ({
+        'sortedZoneKeys': key,
         'datetime': now,
-        'netFlow': parse_flow(x['text2']),
+        'netFlow': parse_flow(x['text2'], is_exporting(non_ch_key, x['direction'])),
         'source': 'swissgrid.ch'
-    }, data)
+    } for x in data if x['id'] == id)
     return list(result)
 
 if __name__ == '__main__':
-    print('fetch_exchange(RU-1, RU-2) ->')
-    print(fetch_exchange('RU-1', 'RU-2'))
+    print('fetch_exchange(CH, FR) ->')
+    print(fetch_exchange('CH', 'FR'))
